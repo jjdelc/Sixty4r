91dfa6689aa3a9bc4231fa753b44dd589fd77eac
Jj <jjdelc@gmail.com>
2011-11-07 11:50:03 -0500

    pep8 and lint love

--- sixty4r.py
+++ sixty4r.py
@@ -3,7 +3,7 @@
 """
 Converts a CSS file's url() image rules paths to base64 data URIs in order to
 minimize HTTP requests.
-Similar effect can be achieved using CSS Sprites, but they are a pain to 
+Similar effect can be achieved using CSS Sprites, but they are a pain to
 maintain
 """
 
@@ -12,10 +12,30 @@ import sys
 from urllib import urlopen
 from os.path import abspath, dirname, join, realpath, splitext
 
+
 class TooBigFileException(Exception):
+    """ This exception will be raised if the to be converted file exceeds
+    the threshold file size """
     pass
 
+
+def get_data(path):
+    """ Returns the resources data, either by opening it or fetching it from
+    a url """
+    if path.startswith('http://'):
+        # Fetch web resource
+        bdata = urlopen(path).read()
+    else:
+        # Open local fs file
+        bdata = open(path).read()
+
+    return bdata
+
+
 class CssConvert(object):
+    """ This class takes a CSS filename and will convert all url() paths
+    into b64 encoded data uris as long as they are smaller than a threshold """
+
     RULE_URL_RE = re.compile('\((?P<path>.+)\)')
     MIME_TYPES = {
         '.gif': 'image/gif',
@@ -24,21 +44,20 @@ class CssConvert(object):
         '.jpeg': 'image/jpeg',
     }
 
-    RES_SIZE_THRESHOLD = 2*1024 # 2KB
+    RES_SIZE_THRESHOLD = 2 * 1024  # 2KB
 
-    def __init__(self, in_fname, out_fname):
-       self.in_fname = in_fname
-       self.out_fname = out_fname
-       self.base_path = self.calculate_base_path(self.in_fname)
+    def __init__(self, input_fname, output_fname):
+        self.in_fname = input_fname
+        self.out_fname = output_fname
+        self.base_path = dirname(abspath(self.in_fname))
 
     def write_output(self):
-        fh = open(self.out_fname, 'w')
+        output = open(self.out_fname, 'w')
         for converted_line in self.parse():
-            fh.write(converted_line)
+            output.write(converted_line)
 
     def parse(self):
-        fh = open(self.in_fname)
-        for rule in fh:
+        for rule in open(self.in_fname):
             yield self.convert_rule(rule)
 
     def convert_rule(self, rule):
@@ -52,8 +71,8 @@ class CssConvert(object):
         return url_path
 
     def is_supported(self, url_path):
-        base, ext = splitext(url_path) # Get extension
-        return ext in self.MIME_TYPES        
+        base, ext = splitext(url_path)  # Get extension
+        return ext in self.MIME_TYPES
 
     def extract_url_path(self, rule):
         return self.RULE_URL_RE.search(rule).groupdict()['path']
@@ -74,32 +93,18 @@ class CssConvert(object):
 
         return rule.replace(url_path, b64data)
 
-    def get_data(self, path):
-        if path.startswith('http://'):
-            # Fetch web resource 
-            bdata = urlopen(path).read()
-        else:
-            # Open local fs file
-            bdata = open(path).read()
-
-        return bdata
-
     def get_b64_datauri(self, path):
-        bdata = self.get_data(path)
+        bdata = get_data(path)
 
         if len(bdata) > self.RES_SIZE_THRESHOLD:
             raise TooBigFileException
 
         # Convert to one line string
         line_bdata = ''.join(bdata.encode('base64').split('\n'))
-        base, ext = splitext(path) # Get extension
+        base, ext = splitext(path)  # Get extension
         mime_type = self.MIME_TYPES[ext]
         return 'data:%s;base64,%s' % (mime_type, line_bdata)
- 
-    def calculate_base_path(self, filename):
-        return dirname(abspath(filename))
 
-       
 
 if __name__ == '__main__':
     in_fname = sys.argv[1]
@@ -107,4 +112,3 @@ if __name__ == '__main__':
 
     css = CssConvert(in_fname, out_fname)
     css.write_output()
-
